faf898eb2cab9e91d829c1acb428ecd8432cd510
Blake Mizerany <blake.mizerany@gmail.com>
2009-10-27 23:02:24 -0700

    refactored

--- echo.rb
+++ echo.rb
@@ -1,11 +1,9 @@
-  def echo(msg)
-    Class.new(Struct.new(:msg)) {
-      def >(fname)
-        open(fname, 'w') {|f| f << msg }
-      end
-
-      def >>(fname)
-        open(fname, 'w+') {|f| f << msg }
-      end
-    }.new(msg + "\n")
-  end
+def echo(msg)
+  Class.new(Struct.new(:msg)) {
+    def >(fname)  ; write(fname, 'w')  ; end
+    def >>(fname) ; write(fname, 'w+') ; end
+    def write(fname, mode)
+      open(fname, mode) {|f| f << msg }
+    end
+  }.new(msg + "\n")
+end
