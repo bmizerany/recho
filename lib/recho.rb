f27e86afa85f38b845892ad083d02132f1e4144f
Blake Mizerany <blake.mizerany@gmail.com>
2009-10-28 00:20:27 -0700

    it's a not w+

--- lib/recho.rb
+++ lib/recho.rb
@@ -1,8 +1,8 @@
 module FileUtils
   def echo(msg)
     msg << "\n"
-    def msg.>(fname)  ; write(fname, 'w')  ; end
-    def msg.>>(fname) ; write(fname, 'w+') ; end
+    def msg.>(fname)  ; write(fname, 'w') ; end
+    def msg.>>(fname) ; write(fname, 'a') ; end
     def msg.write(fname, mode)
       open(fname, mode) {|f| f << self }
       self
